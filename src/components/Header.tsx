0ccb157d9fa13bc55c48b6b4bcfe9945262d0a94
Rodrigo Marques Dias <rodrigo.dias60@etec.sp.gov.br>
2024-12-15 00:39:37 -0300

    fix: ajustes no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,12 +52,12 @@ const HeaderPage = () => {
 
   return (
     <header className={`py-3 xl:py-5 text-white z-50
-        ${pathname === "/" || pathname === "/projetos" ? "sticky top-0" : "fixed top-0 left-0 right-0 z-50"}`}>
+        ${pathname === "/" ? "sticky top-0" : "fixed top-0 left-0 right-0 z-50"}`}>
 
       <div className={`container mx-auto flex justify-between items-center transition-all rounded-2xl p-1.5 xl:p-2.5 ${hasScrolledDown ? "bg-white/[10%]" : "bg-transparent"}`}>
         {/* Logo */}
         <Link href="/">
-          <h1 className="text-4xl font-semibold">
+          <h1 className="text-3xl font-semibold">
             Digo<span className="text-blue-500">.</span>
           </h1>
         </Link>
