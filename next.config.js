dfe048afcea97d50025bc2d647ea794df2f9eca6
digonomundo <rodrigo.dias@digonomundo.com>
2026-06-05 10:11:30 -0300

    fix: IP para view em real time no celular

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     qualities: [75, 90],
   },
-  allowedDevOrigins: ['192.168.0.110', 'localhost:3001']
+  allowedDevOrigins: ['192.168.0.7', 'localhost:3001']
 };
 
 module.exports = nextConfig;
